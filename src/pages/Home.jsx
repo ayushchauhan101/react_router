d9f59d265af4c5381f8099c011759beb37aea59f
ayush <ayush.chauhan0008@gmail.com>
2023-05-18 22:45:03 +0530

    useNavigate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,24 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 // Link components works similar to a href but without re-rendering the website
 function Home() {
+	const navigate = useNavigate()
+
+	// programmatically navigate to products component
+	function navigateHandler() {
+		navigate("/products")
+	}
+
 	return (
 		<>
 			<h1>My homepage</h1>
 			<p>
 				Go to <Link to="/products"> products</Link>
+			</p>
+			<p>
+				<button onClick={navigateHandler}>Navigate to products</button>
+				<br />
+				This button is built using useNavigate()
 			</p>
 		</>
 	)
